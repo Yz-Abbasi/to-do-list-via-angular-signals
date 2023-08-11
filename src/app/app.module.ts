707bbd5cc39b7fa73722e0c8a83f916cfbba5871
Yazdan abbasi <yabasi891@gmail.com>
2023-08-11 23:54:02 +0330

    got rid of the header as it didn't seem necessary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
 import { TaskComponent } from './task/task.component';
 
 @NgModule({
@@ -13,7 +12,6 @@ import { TaskComponent } from './task/task.component';
     AppComponent,
     TodosComponent,
     FooterComponent,
-    HeaderComponent,
     TaskComponent,
   ],
   imports: [
